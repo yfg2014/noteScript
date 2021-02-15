30a71503ff450ceed11680f3811c6a416e635fcd
yfg2014 <2324704583@qq.com>
2021-02-15 23:10:25 +0800

    Create preview.js

---
+++ preview.js
@@ -0,0 +1,14 @@
+var html =
+`
+<script src="https://cdn.jsdelivr.net/npm/texme"><\/script> 
+<style>
+body{
+	font-size:22px;
+	font-family: Microsoft YaHei, Heiti SC, tahoma, arial, Hiragino Sans GB, "宋体", sans-serif ;
+	height: 22px ;
+	line-height: 38px;
+	margin: 20px ;
+}   
+</style>
+`
+document.write(html);
