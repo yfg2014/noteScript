859e47f6ac676454b8acfa550547b7e42e9b9ed2
yfg2014 <2324704583@qq.com>
2021-02-28 15:58:39 +0800

    Update preview.js

--- preview.js
+++ preview.js
@@ -1,6 +1,11 @@
 var html =
 `
+<link href="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/themes/prism.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/components/prism-core.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/plugins/autoloader/prism-autoloader.min.js"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/texme"><\/script> 
+
 <style>
 body{
 	font-size:22px;
