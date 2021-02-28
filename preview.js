d5a5d05c9fe02235735ef811263754f174396080
yfg2014 <2324704583@qq.com>
2021-02-28 16:12:18 +0800

    Update preview.js

--- preview.js
+++ preview.js
@@ -1,10 +1,9 @@
 var html =
 `
+<script src="https://cdn.jsdelivr.net/npm/texme"><\/script> 
 <link href="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/themes/prism.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/components/prism-core.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/plugins/autoloader/prism-autoloader.min.js"></script>
-
-<script src="https://cdn.jsdelivr.net/npm/texme"><\/script> 
 
 <style>
 body{
